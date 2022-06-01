f39c27e50f0414e7db65bc3b11ce594a485d87f8
ZuAnna <eva.anna.ru@gmail.com>
2022-06-01 20:41:55 +0300

    validation step1

--- scripts/script.js
+++ scripts/script.js
@@ -4,3 +4,18 @@ var maskOptions = {
   lazy: false
 };
 var mask = IMask(element, maskOptions);
+
+let reg = /[А-ЯЁ][а-яё]/;
+let regEmail = /^[a-zA-Z0-9][\-_\.\+\!\#\$\%\&\'\*\/\=\?\^\`\{\|]{0,1}([a-zA-Z0-9][\-_\.\+\!\#\$\%\&\'\*\/\=\?\^\`\{\|]{0,1})*[a-zA-Z0-9]@[a-zA-Z0-9][-\.]{0,1}([a-zA-Z][-\.]{0,1})*[a-zA-Z0-9]\.[a-zA-Z0-9]{1,}([\.\-]{0,1}[a-zA-Z]){0,}[a-zA-Z0-9]{0,}$/i;
+
+let surname = document.querySelector('#surname');
+let name = document.querySelector('#name');
+let  pass = document.querySelector('#pass');
+let span = document.querySelector(".span");
+
+document.querySelector('.signin').onclick = function(e){
+  e.preventDefault();
+  if (!regexp.test(surname.value)){
+
+  }
+};
